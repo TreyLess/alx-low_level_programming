9edb9da952a5026b733e7a569ab679d64879c55a
TreyLess <treasureosikwemhe@yahoo.com>
2022-09-09 12:12:15 -0700

    My C program

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,34 +6,28 @@
  */
 int main(void)
 {
-	int i, j, k;
+int digit1, digit2, digit3;
 
-	i = 48;
-	j = 48;
-	k = 48;
-
-	while (i < 58)
+for (digit1 = 0 ; digit1 < 8 ; digit1++)
+{
+	for (digit2 = digit1 + 1; digit2 < 9; digit2++)
 	{
-		j = i + 1;
-		while (j < 58)
+		for (digit3 = digit2 + 1; digit3 < 10; digit3++)
 		{
-			k = j + 1;
-			while (k < 58)
+			putchar((digit1 % 10) + '0');
+			putchar((digit2 % 10) + '0');
+			putchar((digit3 % 10) + '0');
+
+			if (digit1 == 7 && digit2 == 8 && digit3 == 9)
 			{
-				putchar(i);
-				putchar(j);
-				putchar(k);
-				if (i < 55 || j < 56 || k < 57)
-				{
-					putchar(44);
-					putchat(32);
-				}
-				k++;
+				continue;
 			}
-			j++;
+			putchar(',');
+			putchar(' ');
 		}
-		i++;
 	}
-	putchar(10);
-	return (0);
+}
+
+putchar('\n');
+return (0);
 }
