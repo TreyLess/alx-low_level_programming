19ae83586f835ec0f79511fbcb7ddb3904d46091
TreyLess <treasureosikwemhe@yahoo.com>
2022-09-09 01:28:22 -0700

    My puts program on c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
-
 #include <stdio.h>
 /**
- * main- Entry point
+ * main-Entry point for the program
  *
- * Return: Always 0 (Success)
+ * Return: 0 on success
  */
-int main(void);
+int main(void)
 {
 	puts("/"Programming is like building a multilingual puzzle");
 	return (0);
