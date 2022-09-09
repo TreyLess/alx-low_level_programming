baee11e3f71e978763c668981ca947f02ea945f9
TreyLess <treasureosikwemhe@yahoo.com>
2022-09-08 21:27:55 -0700

    c panguage

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main- main
- * Return: 0
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("/")Programming is like building a multilingual puzzle");
+	puts("/"Programming is like builing a multilingual puzzle");
 	return (0);
-;
 }
