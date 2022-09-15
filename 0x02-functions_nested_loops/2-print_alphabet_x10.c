d3ceeb13205dd28dbf5f4ac4fcacc8445619853a
TreyLess <treasureosikwemhe@yahoo.com>
2022-09-14 18:24:46 -0700

    main

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,24 +1,22 @@
 #include "main.h"
-/**
- * print_alphabet_10x - Entry point
- *
- * Return: 0
- */
-void print_alphabet_10x(void)
-{
-	char alpha;
-	int i = 0;
 
-	while (i < 10)
+/**
+ * print_alphabet_x10 - Entry point
+ *Return:Always 0
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char j;
+
+	for (i = 0; i <= 10; i++)
 	{
-		alpha = 'a';
-		while (alpha <= 'z')
+		for (j = 'a'; j <= 'z'; j++)
 		{
-			_putchar (alpha);
-			alpha++;
+			_putchar(j);
 		}
 		_putchar('\n');
-		i++;
 	}
 }
+
 
