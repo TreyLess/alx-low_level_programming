165dacd14bfd40dc5c08ba759182af40d16f66b1
TreyLess <treasureosikwemhe@yahoo.com>
2022-09-14 19:35:43 -0700

    main

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,21 +1,28 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
- * _abs - Entry point
- * @n: the int to print
- * Return: Always 0
+ * _abs - value absolute
+ *
+ * @n: The int to print
+ * Return: Always 0.
  */
+
 int _abs(int n)
 {
-	if (n > 0)
+	if (n < 0)
 	{
 		return (n * (-1));
 	}
+
 	else if (n == 0)
 	{
 		return (0);
 	}
+
 	else
 	{
 		return (n);
 	}
+
 }
